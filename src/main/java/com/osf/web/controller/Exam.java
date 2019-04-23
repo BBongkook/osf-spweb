d983469f71384797b6c5575a3bcd86d193777edf
BBongkook <plmqaz6153@gmail.com>
2019-04-23 10:42:33 +0900

    init

--- src/main/java/com/osf/web/controller/Exam.java
+++ src/main/java/com/osf/web/controller/Exam.java
@@ -1,12 +1,27 @@
 package com.osf.web.controller;
 
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
 public class Exam {
-	public String name;
-	
+	public String str="11";
+	/*
+	 * Log4j level 순서
+	 * 1. TRACE
+	 * 2. DEBUG
+	 * 3. INFO
+	 * 4. WARN
+	 * 5. ERROR
+	 * 6. FATAL
+	 * 
+	 */
 	public static void main(String[] args) {
-		Exam e = new Exam();
-		if(e.name.equals("홍길동")) {
-			System.out.println("이름은 홍길동이 맞습니다.");
+		String str = null;
+		if("홍길동".equals(str)) {
+			log.info("이름은 홍길이 맞습니다.");
+		}else {
+			log.debug("나도 나올려나?");
+			log.info("이름은 홍길이 아닙니다.");
 		}
+		log.warn("프로그램 비정상 종료, 그치만 문제는 없음");
 	}
 }
