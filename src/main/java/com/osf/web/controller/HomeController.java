d414ff3c966bc42cf6ebeb029211a749ac08a42f
BBongkook <plmqaz6153@gmail.com>
2019-04-23 13:22:24 +0900

    pm0122 init

--- src/main/java/com/osf/web/controller/HomeController.java
+++ src/main/java/com/osf/web/controller/HomeController.java
@@ -1,14 +1,30 @@
 package com.osf.web.controller;
 
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class HomeController {
+	@Autowired
+	private DataSource ds;
 	
 	@RequestMapping("/")
 	public String goHome() {
+		try {
+			log.info("con=>{}",ds.getConnection());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return "index";
 	}
 	@RequestMapping("/test")
